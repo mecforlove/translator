f626484bbf93d9c4b0220e25000af2fee171f1d6
zhangguangchao <zhangguangchao@youzan.com>
2017-04-19 00:09:18 +0800

    Completion of the functional development.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: mec
+from setuptools import setup
+
+
+__version__ = '0.0.1'
+
+setup(
+	name='Translator',
+    version=__version__,
+    url='https://github.com/mecforlove/translator',
+    license='Apache',
+    author='mec',
+    author_email='mecforlove@outlook.com',
+    description='A command line translator powered by Youdao api',
+    packages=['translator'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    entry_points='''
+        [console_scripts]
+        translator=translator:main
+    '''
+)
+
---
+++ translator/translator.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python  
+# -*- coding:utf-8 -*-  
+# File Name: translator.py
+# Author: meczhang
+# Mail: mecforlove@outlook.com
+# Created Time: 2016-08-11 19:03:20
+import sys
+import urllib2
+import json
+import urllib
+import readline
+
+# config
+YOUDAO_URL = 'http://fanyi.youdao.com/openapi.do?'
+KEYFORM = 'letusgo'
+KEY = '68421793'
+
+
+def translate(text):
+    """翻译接口调用
+
+    :param text: 待翻译的文本
+    :type text: str
+    :return: api原始响应的dict
+    """
+    req_params = dict(
+        keyfrom=KEYFORM,
+        key=KEY,
+        type='data',
+        doctype='json',
+        version='1.1',
+        q=text
+    )
+    url = YOUDAO_URL + urllib.urlencode(req_params)
+    resp = urllib2.urlopen(url=url)
+    data = json.loads(resp.read())
+    return data
+
+
+class View(object):
+    """表现层
+    ~~~~~~~~~~~~~~~~~
+    用于翻译结果的展示
+    """
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def translation(self):
+        return ', '.join(self.data['translation'])
+
+    @property
+    def basic(self):
+        if 'basic' not in self.data:
+            return ''
+        basic_dict = self.data['basic']
+        explains = '\n'.join(basic_dict.get('explains', ''))
+        return u"""\
+美音：[{us_phonetic}]
+英音：[{uk_phonetic}]
+更多释义：
+{explains}""".format(us_phonetic=basic_dict.get('us-phonetic', ''),
+                     uk_phonetic=basic_dict.get('uk-phonetic', ''),
+                     explains=explains)
+
+
+def main():
+    args = sys.argv
+    if len(args) > 1:
+        text = ''.join(args[1:])
+        view = View(translate(text))
+        print view.translation
+        print '---------------------'
+        print view.basic
+        sys.exit(0)
+    print 'Enter your text to be translated.Type `q` to quit.'
+    while True:
+        text = raw_input('\033[36m>>\033[0m')
+        if text == 'q':
+            print 'Bye!'
+            sys.exit(0)
+        view = View(translate(text))
+        print view.translation
+        print '---------------------'
+        print view.basic
+
+
+
+
+if __name__ == '__main__':
+    main()
